d422b12f34da55c082ecab075ce2b6c08abcccf7
emvmontiel <emvmontiel@gmail.com>
2026-05-02 11:29:51 +0800

    Update main.js

--- javascript-in-a-hurry-initial-main/main.js
+++ javascript-in-a-hurry-initial-main/main.js
@@ -7,3 +7,7 @@ document.querySelector("#open-nav-menu").addEventListener("click", function(){
 document.querySelector("#close-nav-menu").addEventListener("click", function(){ 
     document.querySelector("header nav .wrapper").classList.remove("nav-open");  
 });
+
+function celsiusToFahr(temperature) {
+    console.log(temperature)
+}
