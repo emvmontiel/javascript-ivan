ad5849cd34e5475755da2d828be787512252a548
emvmontiel <emvmontiel@gmail.com>
2026-05-21 17:42:06 +0800

    initiated javascript playground

---
+++ javascript-in-a-hurry-initial-main/playground.js
@@ -0,0 +1,7 @@
+// Personal Task for Fun
+// Create something that allows the greeting to change every time the user reloads the page
+// Format must be: "Hello, user<x092p3>!" where the characters enclosed with <> are dynamic
+// Bonus round: add a greeting tailored to the current time (e.g. morning, noon, evening)
+
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA TIME CHECK 16:40
+
