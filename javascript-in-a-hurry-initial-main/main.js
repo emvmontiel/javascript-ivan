36e89d1578edc860b60dec2fc05ac780c636f528
emvmontiel <emvmontiel@gmail.com>
2026-05-21 17:42:14 +0800

    Update main.js

--- javascript-in-a-hurry-initial-main/main.js
+++ javascript-in-a-hurry-initial-main/main.js
@@ -11,3 +11,17 @@ document.querySelector("#close-nav-menu").addEventListener("click", function(){
 function celsiusToFahr(temperature) {
     console.log(temperature)
 }
+
+// document.querySelector(".weather-group").addEventListener("click", function(e){ 
+//     if(e.target.id == "celsius")    {
+//         console.log("clicked the celsius");
+//     } else if ({
+
+//     }
+// }); 
+
+console.log("My age is " + 24);
+console.log("My age is ", 24);
+const age = 24;
+// console.log("My age is " + age);
+// console.log("My age is ", age);
