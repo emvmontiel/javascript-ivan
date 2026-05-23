7fbd3740eda5be5b62e566855eac579707cdb005
emvmontiel <emvmontiel@gmail.com>
2026-05-23 21:12:19 +0800

    starter personal task 001

--- javascript-in-a-hurry-initial-main/playground.js
+++ javascript-in-a-hurry-initial-main/playground.js
@@ -5,3 +5,30 @@
 
 // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA TIME CHECK 16:40
 
+// document.getElementById("greeting").innerHTML = dynamicGreeting();
+
+function timeGreeting() {
+    const date = new Date();
+    let hour = date.getHours();
+    let greeting = '';
+    switch (hour) {
+        case 5 <= hour <= 11:
+            greeting = 'Good Morning, ';
+            break;
+        case (12 <= hour <= 17):
+            greeting = 'Good Afternoon, ';
+            break;
+        case (18 <= hour <= 23):
+            greeting = 'Good Evening, ';
+            break;
+        default:
+            greeting = 'Hi there, night owl, ';
+            break;
+    }
+    console.log(greeting)
+    console.log(hour)
+    console.log(typeof(hour))
+
+}
+
+timeGreeting();
