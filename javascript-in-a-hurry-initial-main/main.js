2ed3536b203989d7f09e9aebeacc26dd4c99d957
emvmontiel <emvmontiel@gmail.com>
2026-06-04 17:55:42 +0800

    event listener e.target.id

--- javascript-in-a-hurry-initial-main/main.js
+++ javascript-in-a-hurry-initial-main/main.js
@@ -15,10 +15,25 @@ document.querySelector("#close-nav-menu").addEventListener("click", function(){
 // Temperature
 // The weather is cloudy in London and it's 22°C outside. 
 
-const greetingText = "Good Morning!";
+const greetingText = "Ohayo!";
 const weatherCondition = "sunny";
 const userLocation = "New York";
 let temperature = 22.8673;
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${Math.floor(temperature)}°C outside.`
+// let weatherText;
+
+function celsiusToFahr(temperature) {
+    let fahr = (temperature * 1.8) + 32
+    return fahr
+}
+
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText;
+
+document.querySelector(".weather-group").addEventListener("click", function(e) {
+    // console.log(e.target.id) // outputs either celsius or fahr
+    if(e.target.id == "fahr") {
+        weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(Math.floor(temperature))}°F outside.`
+    } else {
+        weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${Math.floor(temperature)}°C outside.`
+    }
+    document.querySelector("p#weather").innerHTML = weatherText;
+});
