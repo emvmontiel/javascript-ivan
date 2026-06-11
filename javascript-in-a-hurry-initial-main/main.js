55da4ac02e1b8c7c48562ff6314bb3fb1cf69e9b
emvmontiel <emvmontiel@gmail.com>
2026-06-11 13:52:57 +0800

    for in challenge done

--- javascript-in-a-hurry-initial-main/main.js
+++ javascript-in-a-hurry-initial-main/main.js
@@ -47,3 +47,23 @@ setInterval(function() {
     document.querySelector('[data-time="seconds"]').innerHTML = new Date().getSeconds().toString().padStart(2, "0");
 }, 1000
 )
+
+/* Consider the following array:
+
+let bankAccounts = ["EX_983746", "US_233478", "UK_098765", "EX_098777", "PT_002544"];
+
+Run a For Loop through the array and send to the console only the accounts with the "EX" prefix.
+
+In the above example, these values must be sent to the console:
+
+"EX_983746"
+"EX_098777" */
+
+let bankAccounts = ["EX_983746", "US_233478", "UK_098765", "EX_098777", "PT_002544"];
+
+for (a in bankAccounts) {
+    let sliced = bankAccounts[a].slice(0, 3);
+    if (sliced == "EX_") {
+        console.log(bankAccounts[a])
+    }
+}
